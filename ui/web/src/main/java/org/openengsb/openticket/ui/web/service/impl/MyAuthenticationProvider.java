3cef12d329a2d11e828ee964c408b78823e41c7d
Michael Heil <michael.heil@chello.at>
2010-11-01 17:27:44 +0100

    styling finished
    Signed-off-by: Michael Heil <michael.heil@chello.at>

--- ui/web/src/main/java/org/openengsb/openticket/ui/web/service/impl/MyAuthenticationProvider.java
+++ ui/web/src/main/java/org/openengsb/openticket/ui/web/service/impl/MyAuthenticationProvider.java
@@ -43,5 +43,4 @@ public class MyAuthenticationProvider extends AbstractUserDetailsAuthenticationP
     public void setService(UserDetailsService service) {
         this.service = service;
     }
-
 }
