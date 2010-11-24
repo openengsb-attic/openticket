f82f87b0f5dd156c8d497c9329de5eda78a159a2
Lukas Puschmann <lukas.puschmann@gmx.at>
2010-11-24 01:34:03 +0100

    Add correct Links to Overview Panel
    Signed-off-by: Lukas Puschmann <lukas.puschmann@gmx.at>

--- app/src/main/java/org/openengsb/openticket/ui/web/OverviewPanel.java
+++ app/src/main/java/org/openengsb/openticket/ui/web/OverviewPanel.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.PageParameters;
 import org.apache.wicket.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
@@ -12,6 +13,7 @@ import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.Filte
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.FilteredAbstractColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.GoAndClearFilter;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.TextFilteredPropertyColumn;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.repeater.Item;
@@ -30,13 +32,8 @@ public class OverviewPanel extends BasePage {
     public OverviewPanel() {
         List<IColumn<Ticket>> columns = new ArrayList<IColumn<Ticket>>();
 
-        add(new Link("createLink") {
-
-            public void onClick() {
-                // TODO: ADD Link to CreatePage
-            }
-
-        });
+        BookmarkablePageLink link = new BookmarkablePageLink("createLink", CreateTicketPage.class);
+        add(link);
 
         IColumn actionsColumn = new FilteredAbstractColumn<Ticket>(Model.of("Actions")) {
 
@@ -71,15 +68,10 @@ public class OverviewPanel extends BasePage {
             super(id);
             final String ticketId = ticket.getId();
 
-            add(new Link("editLink") {
-                /**
-                 * Go to the Edit page, passing this page and the id of the Contact involved.
-                 */
-                public void onClick() {
-                    // TODO:set Link to Editsite with ticketId
-                }
-
-            });
+            PageParameters parameters = new PageParameters();
+            parameters.put("ticket", ticketId);
+            BookmarkablePageLink link = new BookmarkablePageLink("editLink", TicketDetailPage.class, parameters);
+            add(link);
         }
 
     }
