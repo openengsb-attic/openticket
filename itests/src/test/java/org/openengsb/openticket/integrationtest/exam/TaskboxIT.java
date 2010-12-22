a79e7589fe5e11141ab42418b5c0bb64dbdab2a6
Michael Heil <michael.heil@chello.at>
2010-12-22 11:14:23 +0100

    made integrationtest run
    Signed-off-by: Michael Heil <michael.heil@chello.at>

---
+++ itests/src/test/java/org/openengsb/openticket/integrationtest/exam/TaskboxIT.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.openticket.integrationtest.exam;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openengsb.core.common.context.ContextCurrentService;
+import org.openengsb.core.common.taskbox.TaskboxService;
+import org.openengsb.core.common.workflow.RuleManager;
+import org.openengsb.openticket.integrationtest.util.AbstractExamTestHelper;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+
+@RunWith(JUnit4TestRunner.class)
+public class TaskboxIT extends AbstractExamTestHelper {
+    private TaskboxService taskboxService;
+    private RuleManager ruleManager;
+
+    @Before
+    public void setUp() throws Exception {
+        super.beforeClass();
+
+        ContextCurrentService contextService = getOsgiService(ContextCurrentService.class);
+        if (!contextService.getAvailableContexts().contains("openticket-it")) {
+            contextService.createContext("openticket-it");
+            contextService.setThreadLocalContext("openticket-it");
+            contextService.putValue("domain/AuditingDomain/defaultConnector/id", "auditing");
+        } else {
+            contextService.setThreadLocalContext("openticket-it");
+        }
+
+        ruleManager = getOsgiService(RuleManager.class);
+        taskboxService = getOsgiService(TaskboxService.class);
+    }
+
+    @Test
+    public void test_Name() {
+
+    }
+}
